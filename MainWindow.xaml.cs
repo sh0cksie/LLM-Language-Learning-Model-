011930a7f2b7f22d3c868844f3d46c6bf906a9f8
bayas <bayas@DESKTOP-50P05KP>
2025-09-30 21:27:17 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Baseapp.Resources;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Baseapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new MainPage());
+        }
+
+
+        private void PopUP(object sender, RoutedEventArgs e)
+        {
+            AddWordWindow addWindow1 = new AddWordWindow();
+            addWindow1.Show();
+        }
+
+        private void TamirPopUp(object sender, RoutedEventArgs e)
+        {
+            TamirWindow addWindow2 = new TamirWindow();
+            addWindow2.Show();
+        }
+    }
+}
