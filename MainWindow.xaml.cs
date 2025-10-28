214a079e50048b68b218d1d171e95f330c3aa51c
sh0cksie <bayasaa.davka@gmail.com>
2025-10-28 20:54:44 +0800

    Reversed custom title bar
    Custom title bar using wpf is the worst decision i could've made

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -52,28 +52,5 @@ namespace Baseapp
         {
 
         }
-
-        ///Custom windows bar
-        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
-        }
-
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = (this.WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
-        }
-
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
     }
 }
